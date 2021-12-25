0bf4e3cf9998f70b33caa3915ecbfa72929ee87c
stasys-hub <github@stasys.org>
2021-12-25 16:16:11 +0100

    added Fterm configurration

--- lua/user/other_plugins.lua
+++ lua/user/other_plugins.lua
@@ -1,13 +1,20 @@
- --- floating term
+ -- floating term
 require'FTerm'.setup({
+    -- borderlook
     border = 'double',
+    -- transperancy
+    blend = 0,
     dimensions  = {
         height = 0.9,
         width = 0.9,
     },
 })
-vim.cmd('command! FTermOpen lua require("FTerm").open()')
 
---- gut intergration
+-- Create Commands for floatig Terminal
+vim.cmd('command! FTermOpen lua require("FTerm").open()')
+vim.cmd('command! FTermClose lua require("FTerm").close()')
+
+
+-- git intergration
 require('gitsigns').setup()
 
